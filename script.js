894dd6c48f1bdf27bdb837a6a3af9a6287b81bde
Ruben <rubhvi@kvn.no>
2025-01-31 10:35:56 +0100

    Updated
    Probably the last update

---
+++ script.js
@@ -0,0 +1,11 @@
+let shownStory = document.getElementById('shownStory')
+let playerQuestion = document.getElementById('playerQuestion')
+// Options text
+let option1Txt = document.getElementById('option1Txt')
+let option2Txt = document.getElementById('option2Txt')
+let option3Txt = document.getElementById('option3Txt')
+// Options button
+let option1Btn = document.getElementById('option1Btn')
+let option2Btn = document.getElementById('option2Btn')
+let option3Btn = document.getElementById('option3Btn')
+
